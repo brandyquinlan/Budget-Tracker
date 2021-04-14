d2eca1ca5dae58dbb153b7688614a147a66a23ba
Brandy Quinlan <brandyquinlan.bootcamp@gmail.com>
2021-04-14 12:49:09 -0500

    update mongoose connection to include MONGODB_URI

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const dotenv = require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,10 +16,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
+
+console.log(process.env.DB_HOST)
+console.log(process.env.DB_USER)
+console.log(process.env.DB_PASS)
 
 // routes
 app.use(require("./routes/api.js"));
